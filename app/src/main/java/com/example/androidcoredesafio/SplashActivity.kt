a9c969571fbb53910f75f7ba877716d3c4a85cd1
Italo Jamison Félix Monteiro <42391885+ItaloJamison@users.noreply.github.com>
2020-11-08 02:11:17 -0300

    Adicionando tela de splash

---
+++ app/src/main/java/com/example/androidcoredesafio/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.androidcoredesafio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import com.example.androidcoredesafio.ui.login.LoginActivity
+
+
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        val runnable = {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        // depois de 5 segundos vai para tela de login
+        Handler(Looper.getMainLooper()).postDelayed(runnable,5000)
+
+    }
+}
